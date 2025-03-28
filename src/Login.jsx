828f9b1bced087d4743b1b9c9a8ff14fa6d0379f
SunnyPavani <sunnypavani199s@gmail.com>
2025-03-28 03:48:26 -0700

    url path back end

--- src/Login.jsx
+++ src/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("http://localhost:5000/api/auth/login", formData);
+            const res = await axios.post("https://backend-olxlogin.onrender.com//api/auth/login", formData);
             toast.success("Login successful");
             localStorage.setItem("token", res.data.token);
 
